2877d2af5fa6e7ee53385750b4f3a978e325b03f
Trevor Hess <trevor.ghess@gmail.com>
2018-08-15 19:13:01 +0000

    successfully runs, needs core/sql running, still need authdb

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -6,7 +6,7 @@ $pkg_license=@("Apache-2.0")
 $pkg_source="http://download.microsoft.com/download/7/2/8/728F8794-E59A-4D18-9A56-7AD2DB05BD9D/MovieApp_CS.zip"
 # $pkg_filename="$pkg_name-$pkg_version.zip"
 $pkg_shasum="93bb6654357446cd443d75562949ad62194bac07a589b22cd95f2223292c61d0"
-$pkg_deps=@("core/dsc-core")
+$pkg_deps=@("core/dsc-core", "core/nuget")
 $pkg_build_deps=@("core/visual-build-tools-2017", "core/dsc-core", 'core/aspnet-mvc1')
 # $pkg_lib_dirs=@("lib")
 # $pkg_include_dirs=@("include")
@@ -18,9 +18,9 @@ $pkg_build_deps=@("core/visual-build-tools-2017", "core/dsc-core", 'core/aspnet-
 #   ssl-port="srv.ssl.port"
 # }
 # $pkg_exposes=@("port", "ssl-port")
-# $pkg_binds=@{
-#   database="port host"
-# }
+$pkg_binds=@{
+  database="port username password"
+}
 # $pkg_binds_optional=@{
 #   storage="port host"
 # }
@@ -52,16 +52,20 @@ function Invoke-Build{
         
     }
     $proj.Save($csprojPath)
-
-
-    Write-Host $(Get-HabPackagePath aspnet-mvc1)
     
-    MSBuild.exe $csprojPath /property:Configuration=Release
+    MSBuild.exe $csprojPath /property:Configuration=Release 
 }
 
 function Invoke-Install{
+    nuget install Microsoft.SqlServer.SqlManagementObjects -Version 140.17279.0
+    nuget install System.Data.SqlClient -Version 4.4.2
+    Unblock-File "C:\Users\azureadmin\.nuget\packages\microsoft.sqlserver.sqlmanagementobjects\140.17279.0\lib\netstandard2.0\Microsoft.SqlServer.Smo.dll"
+    Unblock-File "C:\Users\azureadmin\.nuget\packages\microsoft.sqlserver.sqlmanagementobjects\140.17279.0\lib\net40\Microsoft.SqlServer.Smo.dll"
+    Unblock-File "C:\Users\azureadmin\.nuget\packages\system.data.sqlclient\4.4.2\lib\netstandard1.2\System.Data.SqlClient.dll"
+
     New-Item -ItemType Directory -Path $pkg_prefix/MovieApp
     Write-Host "$HAB_CACHE_SRC_PATH/$pkg_dirname/src/MovieApp/MovieApp/bin/"
+    Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/src/MovieApp/MovieApp/App_Data" "$pkg_prefix/MovieApp" -recurse
     Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/src/MovieApp/MovieApp/Scripts" "$pkg_prefix/MovieApp" -recurse
     Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/src/MovieApp/MovieApp/Content" "$pkg_prefix/MovieApp" -recurse
     Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/src/MovieApp/MovieApp/bin/" "$pkg_prefix/MovieApp" -recurse
